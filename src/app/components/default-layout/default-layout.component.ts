9683dc7f5d4d9b2e0508dc737560c7319d01b764
leandro.filie <leandro.gfilie@gmail.com>
2025-08-31 17:20:41 -0300

    feat: create default layout

--- src/app/components/default-layout/default-layout.component.ts
+++ src/app/components/default-layout/default-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-layout',
@@ -9,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class DefaultLayoutComponent {
 
+  @Input()
+  title = '';
+
+  @Input()
+  primaryBtn = '';
+
+  @Input()
+  secondaryBtn = '';
 }
